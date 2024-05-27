0b97d81212f24a7787f6b145e1283a89926996fc
terrell511 <jamesreytoroba117@gmail.com>
2024-05-27 19:59:31 +0700

    feat: slicing payment booking & completed transaction

--- src/app/booking/[destination]/page.tsx
+++ src/app/booking/[destination]/page.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { InputFile } from "@/components/ui/inputFile";
 import { InputWithLabel } from "@/components/ui/inputWithLabel";
 import Image from "next/image";
 import React from "react";
@@ -23,19 +23,20 @@ export default function page() {
       </div>
 
       {/* Booking information */}
-
-      <div className="mt-14 text-center">
+      {/* <div className="mt-14 text-center">
         <p className="text-4xl text-cyan-800 font-semibold">
           Booking Information
         </p>
         <p className="text-xl text-gray-400 font-light mt-4">
           Please fill up the blank fields below
         </p>
+      </div> */}
+      <div className="mt-14 text-center">
+        <p className="text-4xl text-cyan-800 font-semibold">Yay! Completed</p>
       </div>
 
       {/* Form Booking information */}
-
-      <div className="mt-24 grid grid-cols-2 gap-8">
+      {/* <div className="mt-24 grid grid-cols-2 gap-8">
         <div className="col-span-1 border-r-2 border-0 border-gray-200 pr-12">
           <div className="relative aspect-video">
             <Image
@@ -80,6 +81,73 @@ export default function page() {
             placeholder="Please type here ..."
           />
         </div>
+      </div> */}
+
+      {/* Form Bank Payment */}
+      {/* <div className="mt-24 grid grid-cols-2 gap-8">
+        <div className="col-span-1 border-r-2 border-0 border-gray-200 pl-20">
+          <p className="text-lg text-cyan-800">Transfer Pembayaran:</p>
+          <p className="text-base text-cyan-800 mt-5">
+            Tax: <strong>10%</strong>
+          </p>
+          <p className="text-base text-cyan-800 my-3">
+            Sub total: <strong>$480 USD</strong>
+          </p>
+          <p className="text-base text-cyan-800">
+            Total: <strong>$580 USD</strong>
+          </p>
+
+          <div className="flex gap-4 mt-12">
+            <div className="relative h-[30px] w-[60px]">
+              <Image fill alt="image-bank-bca" src="/logo-bank/logo_bank.jpg" />
+            </div>
+            <div>
+              <p className="text-base text-cyan-900">Bank Central Asia</p>
+              <p className="text-base text-cyan-900">2208 1996</p>
+              <p className="text-base text-cyan-900">BuildWith Angga</p>
+            </div>
+          </div>
+          <div className="flex gap-4 mt-5">
+            <div className="relative h-[30px] w-[60px]">
+              <Image
+                fill
+                alt="image-bank-bca"
+                src="/logo-bank/logo_bank-1.jpg"
+              />
+            </div>
+            <div>
+              <p className="text-base text-cyan-900">Bank Mandiri</p>
+              <p className="text-base text-cyan-900">2208 1996</p>
+              <p className="text-base text-cyan-900">BuildWith Angga</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 space-y-5 pr-[12rem]">
+          <InputFile
+            label="Upload Bukti Transfer"
+            type="file"
+            placeholder="Please type here ..."
+          />
+          <InputWithLabel
+            label="Asal Bank"
+            placeholder="Please type here ..."
+          />
+          <InputWithLabel
+            label="Nama Pengirim"
+            placeholder="Please type here ..."
+          />
+        </div>
+      </div> */}
+
+      <div className="mt-24 flex flex-col items-center justify-center">
+        <div className="relative w-[360px] h-[360px]">
+          <Image fill alt="completed-payment" src="/completed-payment.jpg" />
+        </div>
+
+        <p className="text-gray-400 text-lg w-1/3 text-center">
+          We will inform you via email later once the transaction has been
+          accepted
+        </p>
       </div>
 
       {/* Button Submit */}
---
+++ src/components/ui/inputFile.tsx
@@ -0,0 +1,24 @@
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+type TInputWithLabel = {
+  type?: string;
+  label: string;
+  placeholder: string;
+};
+
+export function InputFile({ type, label, placeholder }: TInputWithLabel) {
+  return (
+    <div className="grid w-full items-center">
+      <Label className="text-base text-cyan-800" htmlFor={type}>
+        {label}
+      </Label>
+      <Input
+        className="text-base w-full py-4 mt-2 text-cyan-800"
+        type={type}
+        id={type}
+        placeholder={placeholder}
+      />
+    </div>
+  );
+}
