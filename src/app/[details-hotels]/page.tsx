4c71c15d1fa0009cd4c8c9a6c73edda154c41fbe
terrell511 <jamesreytoroba117@gmail.com>
2024-06-06 14:02:02 +0700

    feat: added dynamic title

--- src/app/[details-hotels]/page.tsx
+++ src/app/[details-hotels]/page.tsx
@@ -1,6 +1,21 @@
 import DetailsHotel from "@/components/pages/details-hotels/DetailsHotel";
 import MainLayout from "@/layouts/MainLayout";
 
+type PropsParams = {
+  params: {
+    "details-hotels": string;
+  };
+};
+
+export async function generateMetadata({ params }: PropsParams) {
+  const route: string = params["details-hotels"];
+  const title = route.replaceAll("-", " ");
+
+  return {
+    title: `Hotels | ${title}`,
+  };
+}
+
 export default function page() {
   return (
     <MainLayout>
--- src/app/booking/[payments]/page.tsx
+++ src/app/booking/[payments]/page.tsx
@@ -2,6 +2,21 @@ import BookingPayments from "@/components/pages/booking-payments/BookingPayments
 import SecondaryLayout from "@/layouts/SecondaryLayout";
 import React from "react";
 
+type PropsParams = {
+  params: {
+    payments: string;
+  };
+};
+
+export async function generateMetadata({ params }: PropsParams) {
+  const route: string = params.payments;
+  const title = route.replaceAll("-", " ");
+
+  return {
+    title: `Booking Hotel | ${title}`,
+  };
+}
+
 export default function page() {
   return (
     <SecondaryLayout>
